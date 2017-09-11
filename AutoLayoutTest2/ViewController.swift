56169041e6c8d3f0cd17d294b2e4a6b53f3126a7
Arthur Fofanov <tdevaf8@mtb.com>
2017-09-11 13:42:17 -0400

    Initial Commit

---
+++ AutoLayoutTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutTest2
+//
+//  Created by C02QT97HG8WP on 9/11/17.
+//  Copyright © 2017 C02QT97HG8WP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
